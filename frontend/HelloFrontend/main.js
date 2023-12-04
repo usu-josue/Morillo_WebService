8d7ded5f10d898af53db06fcfec356f6233d417e
usu-josue <126061303+usu-josue@users.noreply.github.com>
2023-12-04 17:22:48 -0500

    cambios sobre conexion back y front

--- frontend/HelloFrontend/main.js
+++ frontend/HelloFrontend/main.js
@@ -1,11 +1,22 @@
-let pingButtton = document.querySelector("#pingButton");
+let message = document.querySelector('#message')
+let pingButton = document.querySelector('#pingButton')
 
-pingButtton.addEventListener('click',getPinFromWebService)
+//emparejar al boton con laa funcion
+pingButton.addEventListener('click',getPing)
 
-function getPinFromWebService(){
-  const url="http://localhost:3000/ping"
-
-  fetch(url).then((response) =>{
-    console.log(response)
-  })
+function getPing(){
+    const url ='http://localhost:3000/ping'
+    //encadenar los datos de la url con la respuesta
+    fetch(url)
+    .then((response) => {
+        return response.json()//solicitar respuesta en formato json 
+    })
+    .then((data) => {
+        console.log(data)
+        message.innerHTML=data.message //asiganar la data retornada al message
+    })
+    .catch(function(error){
+        console.log(error)
+        message.innerHTML=`no se puede conectar al servidor ${url}`
+    })
 }
